a40e27397a67d1521d64c68f2749d95ff7a2a1dc
Walter <walter_halm@hotmail.com>
2024-03-23 16:39:12 -0300

    Elevando proyecto

---
+++ src/main/java/com/miportfoliowalter/SpringBoot/Security/Dto/LoginUsuario.java
@@ -0,0 +1,41 @@
+
+package com.miportfoliowalter.SpringBoot.Security.Dto;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+
+
+
+
+
+
+
+
+
+public class LoginUsuario {
+    @NotBlank
+    private String nombreUsuario;
+    @NotBlank
+    private String password;
+    
+    
+    //getter y setter
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
